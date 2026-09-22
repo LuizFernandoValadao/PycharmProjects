3468a992de3a1ccb0cc30cf28c02958c75937e9b
LuizFernandoValadao <l17996021823@gmail.com>
2026-09-21 21:27:52 -0300

    d016

---
+++ CursoemVideoAula/PythonPOO/desafios/d016/d016.py
@@ -0,0 +1,17 @@
+from rich import print
+
+class Funcionario:
+    def __init__(self, nome, setor, cargo):
+        self.nome = nome
+        self.setor = setor
+        self.cargo = cargo
+
+    def apresentacao(self):
+        return f':handshake: Olá, sou [blue]{self.nome}[/blue] e sou {self.cargo} do setor de {self.setor} da empresa Curso em Vídeo'
+
+
+c1 = Funcionario('Maria', 'Administração', 'Diretora')
+print(c1.apresentacao())
+
+c2 = Funcionario('Pedro', 'TI', 'Programador')
+print(c2.apresentacao())
