3f24283f83de7cfb1674c50b3a9f17590ca0a9f9
LuizFernandoValadao <l17996021823@gmail.com>
2026-09-22 21:23:35 -0300

    uptade d016

--- CursoemVideoAula/PythonPOO/desafios/d016/d016.py
+++ CursoemVideoAula/PythonPOO/desafios/d016/d016.py
@@ -1,13 +1,16 @@
 from rich import print
+from rich import inspect
 
 class Funcionario:
+    # Atributos de Classe
+    empresa = "Curso em Vídeo"
     def __init__(self, nome, setor, cargo):
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
 
-    def apresentacao(self):
-        return f':handshake: Olá, sou [blue]{self.nome}[/blue] e sou {self.cargo} do setor de {self.setor} da empresa Curso em Vídeo'
+    def apresentacao(self) -> str:
+        return f':handshake: Olá, sou [blue]{self.nome}[/blue] e sou {self.cargo} do setor de {self.setor} da empresa {Funcionario.empresa}'
 
 
 c1 = Funcionario('Maria', 'Administração', 'Diretora')
